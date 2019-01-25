6ba8030d3fc9a6d20599949c62e64cd3e3bfdff5
unknown <nnamadimuche@gmail.com>
2019-01-25 10:36:42 +0100

    add reset and delete button

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -12,19 +12,41 @@ class Counters extends Component {
         ]
     };
 
+    handleIncrement = counter => {
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index] = {...counter};
+        counters[index].value++;
+        this.setState({ counters });
+        console.log(this.state.counters[index])
+    }
+
+    handleReset = () => {
+        const counters = this.state.counters.map(c => {
+            c.value = 0;
+            return c;
+        });
+        this.setState({ counters })
+    };
+
     handleDelete = (counterId) => {
         const counters = this.state.counters.filter(c => c.id !== counterId);
         this.setState({ counters })
     }
  
     render() {
-        return ( 
+        return (
             <div>
+                <button
+                    onClick={this.handleReset}
+                    className="btn btn-primary btn-sm m-2">Reset
+                </button>
                 {this.state.counters.map(counter => 
                     (<Counter 
                         key = {counter.id} 
                         counter={counter}
                         onDelete= {this.handleDelete}
+                        onIncrement= {this.handleIncrement}
                     />
                 ))}
             </div>
